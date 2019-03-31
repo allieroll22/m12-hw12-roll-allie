463aa842666d1bd80c9c5eba5502221760e5decb
“Allie <allieroll22@ufl.edu>
2019-03-31 15:21:43 -0400

    Create new object vanillaIceCream

--- src/main.js
+++ src/main.js
@@ -10,6 +10,15 @@ class IceCream extends Desserts {
   constructor(type, calories, flavor, scoops) {
     super(type,calories);
     this.flavor = flavor;
-    this.scoop = scoop;
+    this.scoops = scoops;
   }
+
+  includeSpoon () {
+    console.log('Here is your spoon!');
+  }
+
 }
+
+const vanillaIceCream = new IceCream('Ice Cream', 340, 'Vanilla', 3)
+
+console.log(vanillaIceCream);
